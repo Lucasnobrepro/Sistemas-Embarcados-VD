99213316c9e2d4899c012508e574b142a9bcd996
Lucas Nobre <lucasnobrepro2015@gmail.com>
2021-03-19 02:04:35 -0300

    add stepByStep3

---
+++ hla_ii/teste_comunicacao/não usa/CMakeFiles/VIRTUALBUSAMBASSADOR.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniel/workspace/hla_ii/virtualbus/FederateAmbassador.cpp" "/home/daniel/workspace/hla_ii/teste_comunicacao/CMakeFiles/VIRTUALBUSAMBASSADOR.dir/home/daniel/workspace/hla_ii/virtualbus/FederateAmbassador.cpp.o"
+  "/home/daniel/workspace/hla_ii/virtualbus/VirtualBusFederate.cpp" "/home/daniel/workspace/hla_ii/teste_comunicacao/CMakeFiles/VIRTUALBUSAMBASSADOR.dir/home/daniel/workspace/hla_ii/virtualbus/VirtualBusFederate.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "RTI_USES_STD_FSTREAM"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../virtualbus"
+  "/usr/local/include/hla13"
+  "/usr/local/include/libhla"
+  "/usr/local/lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
